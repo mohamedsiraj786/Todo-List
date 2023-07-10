98c0ac5d986e750a89436a20cd78869cfed6b757
mohamedsiraj786 <110327550+mohamedsiraj786@users.noreply.github.com>
2023-07-10 17:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,43 @@ let button = document.querySelector('button');
 
 let todos = [];
 
+// Add a variable for the voice icon
+const voiceIcon = document.getElementById('voice-icon');
+
+// Check if the browser supports speech recognition
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+  // Enable the microphone icon
+  voiceIcon.classList.add('active');
+
+  // Create a new SpeechRecognition instance
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+  // Set the properties for speech recognition
+  recognition.lang = 'en-US'; // Specify the language
+  recognition.continuous = false; // Stop listening when speech ends
+
+  // Add an event listener to the voice icon
+  voiceIcon.addEventListener('click', () => {
+    // Start speech recognition when the icon is clicked
+    recognition.start();
+  });
+
+  // Add an event listener for the recognition result
+  recognition.addEventListener('result', (event) => {
+    const transcript = event.results[0][0].transcript; // Get the recognized transcript
+
+    value.value = transcript; // Set the input value to the recognized transcript
+    button.click(); // Trigger the click event on the "Add" button
+  });
+
+  // Add an event listener for errors
+  recognition.addEventListener('error', (event) => {
+    console.error('Speech recognition error:', event.error);
+  });
+} else {
+  // Disable the microphone icon if speech recognition is not supported
+  voiceIcon.classList.add('disabled');
+}
 
 
 window.onload = () =>{
